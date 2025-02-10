1d223c44d0b3e0e22525ad420386a8ab6cc5ad33
Miroslav Viktorin <miroslav.viktori@seznam.cz>
2025-02-10 16:42:23 +0100

    model user

--- Django_starter/urls.py
+++ Django_starter/urls.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from app_profile import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", views.user_list, name="users"),
+    path(
+        "user_profile/<int:pk>/",
+        views.user_profile,
+        name="user_profile",
+    ),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- app_profile/views.py
+++ app_profile/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+
+from . import models
+
 
 # Create your views here.
+def user_list(request) -> HttpResponse:
+    """All users list"""
+    users_ = models.User.objects.all()
+    context = {"users": users_}
+    return render(
+        request=request,
+        template_name="users.html",
+        context=context,
+    )
+
+
+def user_profile(request, pk) -> HttpResponse | HttpResponseRedirect:
+    """user profile page"""
+    if models.User.objects.get(id=pk):
+        user_: models.User = models.User.objects.get(id=pk)
+        context: dict[str, user_] = {"user": user_}
+        return render(request=request, template_name="user.html", context=context)
+    return redirect("users")
