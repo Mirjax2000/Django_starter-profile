4ade5ae1a8b77d9522f61d03626fb03224d7f12e
Miroslav Viktorin <miroslav.viktori@seznam.cz>
2025-02-10 12:58:05 +0100

    model user

--- app_profile/models.py
+++ app_profile/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    """User table"""
+
+    user_name = models.CharField(
+        max_length=32,
+        null=False,
+        blank=False,
+        unique=True,
+        verbose_name="User display name",
+    )
+
+    class Meta:
+        ordering = ["user_name"]
+
+    def __repr__(self):
+        return f"<User(user_name={self.user_name!r})>"
+
+    def __str__(self):
+        return self.user_name
